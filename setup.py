2f645d2244e051eb2055d34d6fc2a0c2e4125fb6
gopipandit <gopi1035@gmail.com>
2024-03-02 23:29:35 +0530

    added structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Bappy",
+    author_email="gopi_pandit@yahoo.com",
+    packages=find_packages()
+)
